7fa46346985415bf42df182e33d7e9c4f6a33c92
Allan <cr4z3d@gmail.com>
2010-10-21 15:10:15 +0000

    fixed the json data

--- cdn/metadata.rb
+++ cdn/metadata.rb
@@ -1,6 +1,12 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Datapipe"
+maintainer_email "afeid@datapipe.com"
 license          "All rights reserved"
 description      "Installs/Configures cdn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+attribute	'origin',
+  :display_name => "Cache Origin Server",
+  :description => "The FQDN of your origin server",
+  :type => "string",
+  :required => "required",
+  :default => "origin.example.com"
