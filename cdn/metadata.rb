63cbf6421da0b0470ac85e88ce73ba98257373f5
Allan <cr4z3d@gmail.com>
2010-10-21 15:18:56 +0000

    more rightscale conforming

--- cdn/metadata.rb
+++ cdn/metadata.rb
@@ -4,8 +4,11 @@ license          "All rights reserved"
 description      "Installs/Configures cdn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe "cdn::varnish", "Configures the Varnish caching server"
+recipe "cdn::nginx", "Configures nginx to pull/store static content from the origin server"
 attribute	'origin',
   :display_name => "Cache Origin Server",
   :description => "The FQDN of your origin server",
   :type => "string",
-  :required => "required"
+  :required => "required",
+  :recipes => [ 'cdn::nginx' ]
