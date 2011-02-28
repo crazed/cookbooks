001d995c466941fae187728d213a0267e9a249f4
Allan <cr4z3d@gmail.com>
2011-02-28 10:02:16 -0500

    added deploy cookbook

---
+++ deploy/recipes/git_generic.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: git_generic
+#
+
+deploy node.deploy.dir do
+  repo node.deploy.repo
+  branch node.deploy.branch
+  enable_submodules true
+  # remove rails specific stuff
+  @purge_before_symlink = %w{}
+  @create_dirs_before_symlink = %w{}
+  @symlinks = {}
+  @symlinks_before_migrate = {}
+  migrate false
+  action :deploy
+end
