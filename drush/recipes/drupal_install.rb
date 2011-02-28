00d7f32da482bf0ec5ac7763caec38b8635a73d8
Allan <cr4z3d@gmail.com>
2011-02-28 12:02:35 -0500

    changing up install recipe

--- drush/recipes/drupal_install.rb
+++ drush/recipes/drupal_install.rb
@@ -1,6 +1,9 @@
 include_recipe 'drush::default'
 drush_install node.www.document_root do
-  core '7.0'
-  modules 'pathauto'
+  core node.drush.drupal_version
+  modules node.drush.modules if node[:drush][:modules]
+  mysql_user node.mysql.user
+  mysql_password node.mysql.password
+  mysql_hostname node.mysql.hostname
   action :install
 end
